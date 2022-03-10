b5dc5af2995665606f0aaae6bea4a4279f87c52b
iamgauraav <iamgauraav@gmail.com>
2022-03-10 17:43:29 +0530

    Added UC4 : To calculate daily employee wage using switch statement

--- Program.cs
+++ Program.cs
@@ -1,45 +1,44 @@
-﻿
-using System;
+﻿using System;
+
 namespace EmployeeWageCompute
-{
-    public class program
-    {/// <summary>
-     /// UC-3 Add part time Employee and wage.
-     /// </summary>
-     /// <param name="args"></param>
+{/// <summary>
+ ///  UC-4 Solving using Switch Case Statement
+ /// </summary>
+    class Program
+    {
+        //Constants
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int RATE_PER_HOUR = 20;
+
         static void Main(string[] args)
         {
-            //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-
-            //local Variables
+            //local variable
             int empHrs = 0;
-            int empWage = 0;
+            int empWgae = 0;
 
-            //Pre defined random class
+            //pre defined random class
             Random random = new Random();
 
-            //Next method(),0-initial value,3-no. of elements
+            //Next method() 0-initial value,3-number of elements
             int empCheck = random.Next(0, 3);
 
-            //If else if selection statement
-            if (empCheck == IS_PART_TIME)
+            //Switch Selection statement
+            switch (empCheck)
             {
-                empHrs = 4;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-
-            }
-            else
-            {
-                empHrs = 0;
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage: " + empWage);
+            empWgae = empHrs * RATE_PER_HOUR;
+            Console.WriteLine("Employee wage: " + empWgae);
+            Console.ReadLine();
         }
     }
 }
