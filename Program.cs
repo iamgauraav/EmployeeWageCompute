6f84b26c3fcc51b3efaf73bcd1b5286a71f9dc69
iamgauraav <iamgauraav@gmail.com>
2022-03-10 17:29:32 +0530

    Added UC1 : To check employee attendance

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-
 namespace EmployeeWageCompute
-{
-    class Program
+{/// <summary>
+/// UC-1 Employee is present or absent.
+/// </summary>
+    public class program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            //constants
+            int IS_PRESENT = 1;
+
+            //pre defined random class
+            Random random = new Random();
+
+            //Next method() 0-initial value,2-number of elements
+            int empCheck = random.Next(0, 2);
+
+            //If Selection statement
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
             Console.ReadLine();
         }
     }
