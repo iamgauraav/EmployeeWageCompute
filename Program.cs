1379b16c8f472a222de835dc121fc03dcd608e84
iamgauraav <iamgauraav@gmail.com>
2022-03-10 17:38:19 +0530

    Added UC3 : To calculate daily employee wage with part-time

--- Program.cs
+++ Program.cs
@@ -1,16 +1,18 @@
-﻿using System;
+﻿
+using System;
 namespace EmployeeWageCompute
 {
     public class program
     {/// <summary>
-     /// UC-2 Calculate Daily Employee Wages.
+     /// UC-3 Add part time Employee and wage.
      /// </summary>
      /// <param name="args"></param>
         static void Main(string[] args)
         {
-            //constants
-            int IS_FULL_TIME = 1;
-            int RATE_PER_HOUR = 20;
+            //Constants
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
 
             //local Variables
             int empHrs = 0;
@@ -19,21 +21,25 @@ namespace EmployeeWageCompute
             //Pre defined random class
             Random random = new Random();
 
-            //Next method(),0-initial value,2-number of elements
-            int empCheck = random.Next(0, 2);
+            //Next method(),0-initial value,3-no. of elements
+            int empCheck = random.Next(0, 3);
 
-            //If-else Selection statement
-            if (empCheck == IS_FULL_TIME)
+            //If else if selection statement
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
+
             }
             else
             {
                 empHrs = 0;
             }
-            empWage = empHrs * RATE_PER_HOUR;
-            Console.WriteLine("Emp wage: " + empWage);
-
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
 }
