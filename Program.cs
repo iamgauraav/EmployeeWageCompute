3fade4564f0fb0700b943ec3bc440d4e6e2e934a
iamgauraav <iamgauraav@gmail.com>
2022-03-10 17:22:50 +0530

    Added Welcome message to master branch

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageCompute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            Console.ReadLine();
+        }
+    }
+}
