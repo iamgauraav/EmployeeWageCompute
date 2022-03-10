f48200c210eef6a463c073c04d88de9ebc9b6a5f
iamgauraav <iamgauraav@gmail.com>
2022-03-10 18:02:09 +0530

    Added UC6_EmpWagetillConditionReached

--- Program.cs
+++ Program.cs
@@ -1,36 +1,36 @@
 ﻿
-
 using System;
-
 namespace EmployeeWageCompute
-{
-    ///  UC - 5 Calculating Wages for a Month Assuming 20 Working
-    /// </summary>
-    class Program
+{/// <summary>
+/// UC- 6 Calculate wages till a condition of total working hours of 100 or max days of 20 is reached for a month
+/// </summary>
+    public class program
     {
-        //Constants
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-        public const int RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOURS = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
 
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
-            //local variable
+            //local Variablce
             int empHrs = 0;
-            int empWgae = 0;
             int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
 
-            //pre defined random class
-            Random random = new Random();
-
-            //Repitation statement
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
             {
-                //Next method() 0-initial value,3-number of elements
+                totalWorkingDays++;
+
+                //Predefined class for generating random vales
+                Random random = new Random();
+
+                //Next method(),0-initial point,3-no. of elements
                 int empCheck = random.Next(0, 3);
 
-                //Switch Selection statement
+                //Switch Selection statement execution steps are less
                 switch (empCheck)
                 {
                     case IS_PART_TIME:
@@ -43,20 +43,15 @@ namespace EmployeeWageCompute
                         empHrs = 0;
                         break;
                 }
-                empWgae = empHrs * RATE_PER_HOUR;
-                Console.WriteLine("Day" + day + "EmployeeWage is =" + empWgae);
-
-                //totalEmpWage=totalEmpWage+empWage
-                totalEmpWage += empWgae;
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
             }
-            Console.WriteLine("Total Employee wage for {0} days is={1}", NUM_OF_WORKING_DAYS, totalEmpWage);
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOURS;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
             Console.ReadLine();
         }
     }
 }
-
-
-
 
 
 
