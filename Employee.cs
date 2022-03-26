8f5ce165c03b882d1010c578d2f60bfd1bc6fac4
iamgauraav <iamgauraav@gmail.com>
2022-03-26 18:56:12 +0530

    Added UC9 : To check Total Wage for Each Company

---
+++ Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace EmployeeWageCompute
+{
+    public class EmployeeBuilderObject
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public EmployeeBuilderObject (string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        
+        public void computeEmpWage()
+        {
+            // local Variables 
+            int empHrs = 0, totalEmpHr = 0, totalWorkingDays = 0;
+
+            while (totalEmpHr <= this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                //pre defined random values 
+                Random random = new Random();
+                //next method(),0-initial point,4-no. of elements 
+                int empCheck = random.Next(0, 3);
+
+                // Switch Selection statement
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        Console.WriteLine("Employee is Present for Part time");
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        Console.WriteLine("Employe is Present for Full time");
+                        empHrs = 8;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHr += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
+            }
+            totalEmpWage = totalEmpHr + this.empRatePerHour;
+            Console.WriteLine("Total Emp Wage For Company : " + company + "is: " + totalEmpWage);
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company : " + this.company + "is: " + this.totalEmpWage;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,54 +1,20 @@
 ﻿using System;
 namespace EmployeeWageCompute
 {/// <summary>
- /// UC-8 Compute Employee Wagefor multiple companies
+ /// UC-9 Ability to save the Total Wage for Each Company
  /// </summary>
     class EmployeeWage
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-
-        public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-        {
-            // local Variables 
-            int empHrs = 0, totalEmpHr = 0, totalWorkingDays = 0;
-
-            while (totalEmpHr <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                //pre defined random values 
-                Random random = new Random();
-                //next method(),0-initial point,4-no. of elements 
-                int empCheck = random.Next(0, 3);
-
-                // Switch Selection statement
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        Console.WriteLine("Employee is Present for Part time");
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        Console.WriteLine("Employe is Present for Full time");
-                        empHrs = 8;
-                        break;
-                    default:
-                        Console.WriteLine("Employee is Absent");
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHr += empHrs;
-                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
-            }
-            int totalEmpWage = totalEmpHr * empRatePerHour;
-            Console.WriteLine("Total Emp Wage For Company : " + company + "is: " + totalEmpWage);
-            return totalEmpWage;
-        }
         static void Main(String[] args)
         {
-            ComputeEmpWage("DMart", 20, 2, 10);
-            ComputeEmpWage("Reliance", 10, 4, 20);
+            EmployeeBuilderObject dMart = new EmployeeBuilderObject("Dmart", 20, 2, 10);
+            EmployeeBuilderObject reliance = new EmployeeBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
             Console.ReadLine();
+            
         }
     }
 }
